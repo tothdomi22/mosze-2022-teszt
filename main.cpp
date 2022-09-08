cf99595be65af2fc0ce68699165cea92157438e1
tothdomi22 <67698947+tothdomi22@users.noreply.github.com>
2022-09-08 18:57:16 +0200

    Update main.cpp
    Hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //nincs értelme dinamikus tömbnek, mert konstant értékkel dolgozunk + a konstans el van írva
+    std::cout << '1-100 ertekek duplazasa' //nincs ; és "" közé kell rakni
+    for (int i = 0;) //a for ciklus paramétere nem megfelelőek
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //(i+1)-ra van szükség hogy ne 0tol 99ig számoljon
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //nem i-g kell menni hanem N_ELEMENTS-ig
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //endl hozzáadva hogy szebb legyen
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //double kell hogy legyen az átlag és kell neki kezdőértéket adni
+    for (int i = 0; i < N_ELEMENTS, i++) //nem vesszővel hanem ; kell elválasztani
     {
         atlag += b[i]
     }
